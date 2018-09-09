f0283f38aa3097e511db32aab89b5c0937f73241
codinget <nathan.decher@gmail.com>
2018-09-09 11:13:53 +0200

    hopefully fix edt

--- modules/edt.js
+++ modules/edt.js
@@ -7,8 +7,9 @@ shared.commands.edt=function(msg, args) {
 	}
 	let cal=shared.calendars[args[0].toLowerCase()];
 	if(!cal) return msg.reply("Ce calendrier n\'existe pas");
-	let evts=cal.getForWeek();
-	if(args[1]=='today') evts=cal.getForDay();
+	let evts;
+	if(args[1]=='' || args===undefined) evts=cal.getForWeek();
+	else if(args[1]=='today') evts=cal.getForDay();
 	else if(args[1]=='tomorrow') evts=cal.getForDay(new Date(Date.now()+dates.oneDay));
 	else if(args[1]=='next') evts=cal.getForWeek(new Date(Date.now()+dates.oneWeek));
 	else if(args[1].match('[0-3]?[0-9]/[01]?[0-9]/?[0-9]*')) {
@@ -18,7 +19,7 @@ shared.commands.edt=function(msg, args) {
 		date.setMonth(parts[1]-1);
 		date.setDate(parts[0]);
 		evts=cal.getForDay(date);
-	} else if(args[1]!==undefined) {
+	} else {
 		return msg.reply("**ERROR**: wrong date format");
 	}
 	let embed=new Discord.RichEmbed();
