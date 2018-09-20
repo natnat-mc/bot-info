822c19c0d4ee636bbdcc917405e1f6882d7180c4
Nathan DECHER <nathan.decher@gmail.com>
2018-09-20 15:59:29 +0200

    better command parser, if I'm not bad

--- api/commands.js
+++ api/commands.js
@@ -7,10 +7,11 @@ const shared=require('./shared');
 let cmds={}
 shared.commands=cmds;
 
+const reg=/^([^ ]+) (.+)$/m;
+
 function commands(msg) {
-	let text=msg.content.slice(config('bot.prefix').length);
-	let args=text.split(/[\t ]+/g);
-	let cmd=args.shift().toLowerCase();
+	const text=msg.content.slice(config('bot.prefix').length);
+	const {cmd, args}=parse(text);
 	console.log(cmd, args);
 	if(cmd=='ping') {
 		msg.reply('pong '+args.join(' '));
@@ -28,4 +29,99 @@ function commands(msg) {
 	}
 }
 
+/* Parses a command string, removing the prefix and returning the command and arguments.
+ * If the string isn't a command string, returns null.
+ * The values are returned as an object {cmd, args}.
+ * This code allows for several escape sequences:
+ * 	\n => newline
+ * 	\t => tabulation
+ * 	\' => single quote
+ * 	\" => double quote
+ * 	\\ => backslash
+ * 	\  => space
+ * The backslash character by itself is invalid.
+ */
+function parse(str) {
+	// handle special conditions
+	if(!str.startsWith(config('bot.prefix'))) return null;
+	str=str.slice(config('bot.prefix').length);
+	
+	// read command
+	let found=reg.exec(str);
+	if(!found) {
+		return {
+			cmd: str.toLowerCase(),
+			args: []
+		};
+	}
+	const cmd=found[1].toLowerCase();
+	const rawArgs=found[2];
+	
+	// read arguments
+	let args=[];
+	let quote=null;
+	let escaped=false;
+	let current='';
+	console.log(cmd, rawArgs);
+	for(let i=0; i<rawArgs.length; i++) {
+		let char=rawArgs[i];
+		console.log('\''+char+'\'', i);
+		if(escaped) {
+			// handle escape sequences
+			switch(char) {
+				case 'n':
+					current+='\n';
+					break;
+				case 't':
+					current+='\t';
+					break;
+				case '\'':
+					current+='\'';
+					break;
+				case '\"':
+					current+='\"';
+					break;
+				case '\\':
+					current+='\\';
+					break;
+				case ' ':
+					current+=' ';
+					break;
+				default:
+					throw new Error("Unknown escape sequence \"\\"+current+"\"");
+			}
+			escaped=false;
+		} else if(char=='\\') {
+			// backslash starts an escape sequence
+			escaped=true;
+		} else if(char=='\'' || char=='\"') {
+			// handle quotes
+			if(quote==char) {
+				quote=null;
+			} else if(quote) {
+				current+=char;
+			} else {
+				quote=char;
+			}
+		} else if(!quote && (char==' ' || char=='\t')) {
+			// split arguments on space or tabulation
+			if(current) {
+				args.push(current);
+				current='';
+			}
+		} else {
+			current+=char;
+		}
+	}
+	if(current) args.push(current);
+	if(quote) throw new Error("Unmatched quote");
+	if(escaped) throw new Error("Unfinished escape sequence");
+	
+	// return a result
+	return {
+		cmd, args
+	};
+}
+
 module.exports=exports=commands;
+exports.parse=parse;
