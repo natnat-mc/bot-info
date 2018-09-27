6aba26fa36c27111da6993ee552df67033a952c9
Nathan DECHER <nathan.decher@gmail.com>
2018-09-27 16:41:50 +0200

    started working on the OAuth flow

--- modules/webui.js
+++ modules/webui.js
@@ -5,14 +5,21 @@ module.desc="Serves a web UI";
 // load the libraries
 const express=require('express');
 const expressWs=require('express-ws');
+const ClientOAuth2=require('client-oauth2');
+const store=require('./storage');
 
 // create an Express.js web server and configure it
 const app=express();
 expressWs(app);
+const router=express.Router();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('static'));
 app.use(express.json());
 app.use(express.urlencoded());
+
+// treat "/" and "/bot-info" as one
+app.use(router);
+app.use('/bot-info', router);
 
 // make it listen on the configured port
 let server=app.listen(config('webui.internalPort'), () => {
@@ -37,9 +44,55 @@ shared.commands.webui.help={
 	admin: false
 };
 
+// create a OAuth2 client
+const OAuth=new ClientOAuth2({
+	clientId: config('bot.clientId'),
+	clientSecret: config('bot.clientSecret'),
+	accessTokenUri: 'https://discordapp.com/api/oauth2/token',
+	authorizationUri: 'https://discordapp.com/api/oauth2/authorize',
+	redirectUri: config('webui.externalAddr')+'/login/callback',
+	scopes: config('webui.scopes')
+});
+
+// the user's data
+const users={}, tokens=[];
+
+// setup the data store
+store.ensureStore('webui.oauth').then(created => {
+	if(created) {
+		return store.set('webui.oauth', 'users', {}).then(() => {
+			return store.set('webui.oauth', 'tokens', []);
+		});
+	} else {
+		//TODO retrieve tokens and users
+	}
+}).catch(console.error);
+
+// setup the routes
+router.get('/login', (req, res) => {
+	const uri=OAuth.code.getUri();
+	console.log(uri);
+	res.redirect(uri);
+});
+router.get('/login/callback', (req, res) => {
+	OAuth.code.getToken(req.originalUrl).then(token => {
+		console.log(token);
+		return token.refresh();
+	}).then(token => {
+		console.log(token);
+	}).then(token => {
+		//TODO write the token somewhere
+		//TODO push a cookie
+		//TODO redirect somewhere useful
+	}).catch(err => {
+		console.error(err);
+		res.status(500).end('Something went wrong');
+	});
+});
+
 // function to automatically add a view with some context
 function addView(path, file, page) {
-	app.get(path, (req, res) => {
+	router.get(path, (req, res) => {
 		res.render(file, {
 			req,
 			config,
