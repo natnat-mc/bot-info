68a892ad289fbb22cfe7c3f853017780c70c943a
Nathan DECHER <nathan.decher@gmail.com>
2020-03-20 11:44:13 +0100

    added poll command

---
+++ modules/poll.js
@@ -0,0 +1,34 @@
+module.type='command';
+module.desc="Permet de crééer un sondage";
+
+shared.commands.poll=async (msg, args) => {
+	let text="Sondage de "+msg.author+": "+args.shift();
+	args.forEach((t, i) => text+="\n"+String.fromCodePoint(0x1f1e6+i)+": "+t);
+	let poll=await msg.channel.send(text);
+	for(let i in args) {
+		await poll.react(String.fromCodePoint(0x1f1e6+i));
+	}
+};
+
+shared.commands.poll.help={
+	name: 'poll',
+	desc: "Créé un sondage basé sur les réactions",
+	admin: false,
+	category: 'util'
+};
+
+shared.commands.poll.usage=[
+	{
+		name: 'question',
+		required: true,
+		desc: "La question à poser, entourée par des \""
+	}, {
+		name: 'réponses',
+		required: true,
+		desc: "Les réponses possibles, entourées par des \" et séparées par des espaces"
+	}
+];
+
+module.unload=() => {
+	delete shared.commands.poll;
+};
